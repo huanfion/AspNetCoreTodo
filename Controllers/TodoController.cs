b0c4963ffd16d4aa2a7cfa464360edc3f478f826
huanfion <648890146@qq.com>
2018-05-21 12:32:22 +0800

    add

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            // 从数据库获取 to-do 条目
+            var todoItems = await _todoItemService.GetIncompleteItemsAsync();
+            // 把条目置于 model 中
+            var model = new TodoViewModel()
+            {
+                Items = todoItems
+            };
+            // 使用 model 渲染视图
+            return View(model);
+
+        }
+    }
+}
---
+++ Services/FakeTodoItemService.cs
@@ -0,0 +1,32 @@
+﻿
+using AspNetCoreTodo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTodo.Services
+{
+    public class FakeTodoItemService:ITodoItemService
+    {
+        public Task<IEnumerable<TodoItem>> GetIncompleteItemsAsync()
+        {
+            // 返回一个 TodoItems 的数组
+            IEnumerable<TodoItem> items = new[]
+            {
+                new TodoItem
+                {
+                    Title = "Learn ASP.NET Core",
+                    DueAt = DateTimeOffset.Now.AddDays(1)
+                },
+                new TodoItem
+                {
+                    Title = "Build awesome apps",
+                    DueAt = DateTimeOffset.Now.AddDays(2)
+                }
+            };
+
+            return Task.FromResult(items);
+        }
+    }
+}
